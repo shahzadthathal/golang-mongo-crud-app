95371de93c5c871804bd8750682369ed41fc15f6
Muhammad Shahzad <shahzadthathal@gmail.com>
2021-04-17 16:37:50 +0400

    changes

--- controller.go
+++ controller.go
@@ -6,6 +6,11 @@ import (
 	"fmt"
 	"log"
 	"net/http"
+
+	"github.com/gorilla/mux"
+	"go.mongodb.org/mongo-driver/bson"
+	"go.mongodb.org/mongo-driver/bson/primitive"
+	"go.mongodb.org/mongo-driver/mongo/options"
 )
 
 //Struct for storing data
@@ -34,4 +39,78 @@ func createProfile(w http.ResponseWriter, r *http.Request) {
 	fmt.Println("inserted a single document: ", insertResult)
 	json.NewEncoder(w).Encode(insertResult.InsertedID)
 	//return the mongodb id of geenrated document
+}
+
+func getAllUsers(w http.ResponseWriter, r *http.Request) {
+	w.Header().Set("Content-Type", "application/json")
+	var results []primitive.M                                   //slice for multiple documents
+	cur, err := userCollection.Find(context.TODO(), bson.D{{}}) //Returns a *mongo.Cursor
+	if err != nil {
+		fmt.Println(err)
+	}
+
+	for cur.Next(context.TODO()) {
+		var elem primitive.M
+		err := cur.Decode(&elem)
+		if err != nil {
+			log.Fatal(err)
+		}
+		results = append(results, elem)
+	}
+	cur.Close(context.TODO())
+	json.NewEncoder(w).Encode(results)
+}
+
+func updateProfile(w http.ResponseWriter, r *http.Request) {
+
+	w.Header().Set("Content-Type", "application/json")
+
+	type updateBody struct {
+		Name string `json:"name"` //value that has to be matched
+		City string `json:"city"` // value that has to be modified
+	}
+	var body updateBody
+	e := json.NewDecoder(r.Body).Decode(&body)
+	if e != nil {
+
+		fmt.Print(e)
+	}
+	filter := bson.D{{"name", body.Name}} // converting value to BSON type
+	after := options.After                // for returning updated document
+	returnOpt := options.FindOneAndUpdateOptions{
+
+		ReturnDocument: &after,
+	}
+	update := bson.D{{"$set", bson.D{{"city", body.City}}}}
+	updateResult := userCollection.FindOneAndUpdate(context.TODO(), filter, update, &returnOpt)
+
+	var result primitive.M
+	_ = updateResult.Decode(&result)
+
+	json.NewEncoder(w).Encode(result)
+
+}
+
+func deleteProfile(w http.ResponseWriter, r *http.Request) {
+	w.Header().Set("Content-Type", "application/json")
+
+	params := mux.Vars(r)["id"] //Get query params valud as string
+
+	_id, err := primitive.ObjectIDFromHex(params)
+
+	if err != nil {
+		fmt.Printf(err.Error())
+	}
+
+	opts := options.Delete().SetCollation(&options.Collation{})
+	res, err := userCollection.DeleteOne(context.TODO(), bson.D{{"_id", _id}}, opts)
+
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	fmt.Printf("deleted %v documents \n", res.DeletedCount)
+
+	json.NewEncoder(w).Encode(res.DeletedCount)
+
 }
--- main.go
+++ main.go
@@ -16,6 +16,9 @@ func main() {
 
 	//HandleFunc registers a new route with a matcher for the URL path.
 	s.HandleFunc("/createProfile", createProfile).Methods("Post")
+	s.HandleFunc("/get/all/users", getAllUsers).Methods("GET")
+	s.HandleFunc("/updateProfile", updateProfile).Methods("PUT")
+	s.HandleFunc("/delete/profile/{id}", deleteProfile).Methods("DELETE")
 
 	log.Fatal(http.ListenAndServe(":8080", s)) //Run Server
 }
